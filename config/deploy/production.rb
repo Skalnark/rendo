9e8c11cc3aeb0e454b10cbfa0776f2fa750c9ea2
skalnark <skalnark@live.com>
2018-06-30 00:36:40 +0000

    unicorn changed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -67,6 +67,8 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log" 
 stdout_path "#{root}/log/unicorn.log" 
  
-worker_processes 4 timeout 30 preload_app true 
+worker_processes 4 
+timeout 30 
+preload_app true 
  
 listen '/tmp/rod.sock', backlog: 64 
